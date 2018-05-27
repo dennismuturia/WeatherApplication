40f2766778cc92f3f7f2201e2aaa4255e7031a8f
dennismuturia <dmuturia2922@gmail.com>
2018-05-27 16:19:32 +0300

    Managed to provide the current location name on the device

--- app/src/main/java/com/webweaver/dennis/weatherapplication/MainActivity.java
+++ app/src/main/java/com/webweaver/dennis/weatherapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package com.webweaver.dennis.weatherapplication;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.Locale;
@@ -13,15 +20,56 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     LocationManager locationManager;
     TextView locationText;
+    private static final int MY_PERMISSION_REQUEST_LOCATION = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         locationText = (TextView)findViewById(R.id.locationText);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)){
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            }else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+            }
+        }
+        else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
-        getLocation();
+            try {
+                locationText.setText(getLocation(location.getLatitude(), location.getLongitude()));
+            }catch (Exception e){
+                e.printStackTrace();
+                Toast.makeText(this, "Location not found!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST_LOCATION:{
+                if (grantResults.length > 0 && grantResults[0] ==PackageManager.PERMISSION_GRANTED){
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                        try {
+                            locationText.setText(getLocation(location.getLatitude(), location.getLongitude()));
+                        }catch (Exception e){
+                            e.printStackTrace();
+                            Toast.makeText(this, "Location not found!", Toast.LENGTH_SHORT).show();
+                        }
+                    }else {
+                        Toast.makeText(this, "No permission has been granted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
     //In this method we will be getting the city name
     public String getLocation(double latitude, double longitude){
         String currentCity = "";
