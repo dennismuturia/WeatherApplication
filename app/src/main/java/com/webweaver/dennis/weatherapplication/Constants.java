349f1a9abb373177534000c992de4cf9eb28312a
dennismuturia <dmuturia2922@gmail.com>
2018-05-27 12:50:27 +0300

    Added a google dependency for us to get the locations of the current city.

---
+++ app/src/main/java/com/webweaver/dennis/weatherapplication/Constants.java
@@ -0,0 +1,11 @@
+package com.webweaver.dennis.weatherapplication;
+
+/**
+ * Created by dennis on 5/27/18.
+ */
+
+public class Constants {
+    public static String currentWeather ="";
+    public static String forecast ="";
+    public static String apiKey = BuildConfig.apiKey;
+}
