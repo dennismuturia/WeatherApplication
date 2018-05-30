0742b5396143c28b8402457f9ece2784054bcf50
dennismuturia <dmuturia2922@gmail.com>
2018-05-30 21:07:24 +0300

    Created a custom adapter

--- app/src/main/java/com/webweaver/dennis/weatherapplication/adapters/CurrentWeatherAdapter.java
+++ app/src/main/java/com/webweaver/dennis/weatherapplication/adapters/CurrentWeatherAdapter.java
@@ -1,5 +1,22 @@
 package com.webweaver.dennis.weatherapplication.adapters;
 
-public class CurrentWeatherAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.webweaver.dennis.weatherapplication.model.CurrentWeatherModel;
+
+import java.util.ArrayList;
+
+public class CurrentWeatherAdapter extends RecyclerView.Adapter<CurrentWeatherAdapter.CurrentWeatherViewHolder>{
+    private ArrayList<CurrentWeatherModel>weatherModels = new ArrayList<>();
+    private Context mContext;
+
+
+    public CurrentWeatherAdapter(Context context, ArrayList<CurrentWeatherModel>currentWeatherModels){
+        mContext = context;
+        weatherModels = currentWeatherModels;
+    }
 
 }
